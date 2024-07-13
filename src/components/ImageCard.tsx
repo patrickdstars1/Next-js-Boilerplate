8d977b14a54d09a45a9ebc5c0958fe14969c67ea
patrickdstars <patcharapon.khongmod@hotmail.com>
2024-07-13 21:42:41 +0700

    message

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 const ImageCard: React.FC = () => {
   return (
     <div className="bg-indigo-900 py-8 ">
@@ -11,7 +10,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4NzY0ODgzNzUzNjQzNw%3D%3D/original/cda19e4c-ae11-47b1-831b-a29f94ab76dc.png?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -33,7 +32,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/4f7a276e-9995-4b32-bda6-300de0619b25.png?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -55,7 +54,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNjMyMzc5Mzc2MTc3OTEzMg%3D%3D/original/01819c16-43a1-4aee-9957-9edce6eb8e16.png?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -77,7 +76,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/miso/Hosting-881808599061267756/original/b16970cf-1d55-4edd-bb1f-e1735d0a228e.jpeg?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -99,7 +98,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/miso/Hosting-857387972692815761/original/d106e0ef-f825-4ff8-baf7-86256a54fbd5.jpeg?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -121,7 +120,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/miso/Hosting-53274539/original/365299e3-f926-47ee-bcbf-606d6a0370b9.jpeg?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -143,7 +142,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/miso/Hosting-694055224756906854/original/76f85a0c-b3e2-4f1d-9aa9-d7838f2393c6.jpeg?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -164,7 +163,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4MzUyMzk5Mjc3MDU5Nw%3D%3D/original/ced15ffe-0ab5-48cf-a189-dbdeaaf04387.jpeg?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
@@ -185,7 +184,7 @@ const ImageCard: React.FC = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
+              <img
                 src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNTQ0NTEyMzEwMTI3NDg1MQ%3D%3D/original/bd73f0f8-9057-4bbc-ad70-1db13eb5c03f.png?im_w=1440&im_q=highq"
                 alt="Room"
                 className="w-full h-64 object-cover rounded-lg mb-4"
